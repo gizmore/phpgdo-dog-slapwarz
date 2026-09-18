315aea85bc1b5e35c233720330e18a8715cf72c3
gizmore <gizmore@wechall.net>
2026-09-18 04:07:48 +0200

    'more korean'

---
+++ lang/slapwarz_ko.php
@@ -0,0 +1,8 @@
+<?php
+namespace GDO\DogSlapwarz\lang;
+return [
+	'msg_dog_slaps' => '%s %s %s %s 및 %s %s.',
+	'msg_dog_slap_score' => '(%s 피해)',
+	'msg_dog_slap_stats' => '%s님은 다른 사람의 뺨을 %s번(%s포인트) 쳤고 %s번(%s포인트) 뺨을 맞았으며 %s잔류 랩(각각 %s포인트)을 기록했습니다. 총합은 %s (%s) 포인트입니다.',
+	'msg_dog_slap_remain' => '(%s 포인트, %s 남음)',
+];
